b728a503c4e13b378b87d5fc03b83ade2a96d189
94ju <janithherath94@gmail.com>
2019-05-11 00:46:25 +0530

    course end point changed

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const express=require('express');
 const app = express();
-
+const courses=[
+    {id:1,name:'maths'},
+    {id:2,name:'science'},
+    {id:3,name:'english'},
+];
 app.get('/',(req,res)=>{
     res.send('welcome');
 });
 app.get('/api/courses',(req,res)=>{
-    res.send([1,2,3]);
+    res.send(courses);
 });
 
 app.get('/api/courses/:id',(req,res)=>{
-    res.send(req.params.id);
+    const id=parseInt(req.params.id);
+    const course = courses.find(c => c.id===id).name;
+    res.send(course);
 });
 app.get('/api/courses/posts/:year/:month',(req,res)=>{
     res.send(req.params);
