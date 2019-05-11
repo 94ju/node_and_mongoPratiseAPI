5cf0063437024869b8a0e6a61113deb0586f480e
94ju <janithherath94@gmail.com>
2019-05-11 12:14:13 +0530

    post request end point

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express=require('express');
 const app = express();
+app.use(express.json());
 const courses=[
     {id:1,name:'maths'},
     {id:2,name:'science'},
@@ -14,10 +15,22 @@ app.get('/api/courses',(req,res)=>{
 
 app.get('/api/courses/:id',(req,res)=>{
     const id=parseInt(req.params.id);
-    const course = courses.find(c => c.id===id).name;
+    const course = courses.find(c => c.id===id);
+    if(!course){
+        res.status(404).send('The cousres with the given id was not found');
+    }
     res.send(course);
 });
 app.get('/api/courses/posts/:year/:month',(req,res)=>{
     res.send(req.params);
-})
+});
+
+app.post('/api/courses',(req,res)=>{
+    const course ={
+        id:courses.length+1,
+        name:req.body.name
+    };
+    courses.push(course);
+    res.send(course);
+});
 app.listen(3000);
