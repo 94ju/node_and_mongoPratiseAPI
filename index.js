b3eb2530542835811530a242af7c0dc3554ec991
94ju <janithherath94@gmail.com>
2019-05-11 17:48:55 +0530

    object destrructuring

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const courses=[
 app.get('/',(req,res)=>{
     res.send('welcome');
 });
+
 app.get('/api/courses',(req,res)=>{
     res.send(courses);
 });
@@ -27,11 +28,8 @@ app.get('/api/courses/posts/:year/:month',(req,res)=>{
 });
 
 app.post('/api/courses',(req,res)=>{
-    const schema ={
-        name:Joi.string().min(3).required()
-    }
-    const result= Joi.validate(req.body,schema);
-    
+
+    const result =validateCourse(req.body);
     if(result.error){
         res.status(400).send(result.error.details[0].message);
         return;
@@ -43,4 +41,29 @@ app.post('/api/courses',(req,res)=>{
     courses.push(course);
     res.send(course);
 });
+
+app.put('/api/courses/:id',(req,res)=>{
+    //look up the course 
+    const id=parseInt(req.params.id);
+    const course=courses.find(c=>c.id===id);
+    //if not exisit return 404
+    if(!course){
+        res.status(404).send('course not found')
+    }
+    const {error}=validateCourse(req.body);
+    if(error){
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+    //update course
+    course.name=req.body.name;
+    //return the update course
+    res.send(course);
+});
+const validateCourse=(course)=>{
+    const schema={
+        name:Joi.string().min(3).required()
+    }
+    return Joi.validate(course ,schema); 
+}
 app.listen(3000);
